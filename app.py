b4ed129dac53c2121d41c10804757b85ab7ccaa1
robibrutoczki <robibrutoczki@googlemail.com>
2020-05-08 11:54:36 +0000

    Testing Eployment

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if path.exists("env.py"):
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get('MY_DB')
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["MONGO_URI"] = MONGO_URI
 mongo = PyMongo(app)
 datetime_now = datetime.now()  # pass this to a MongoDB doc
 
