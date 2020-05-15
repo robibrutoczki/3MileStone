61d0faf4ef6968924adea2c398445a9bfa5f6eae
robibrutoczki <robibrutoczki@googlemail.com>
2020-05-15 18:09:56 +0000

    Some changes in menu,added modals

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 import os
+import bcrypt
 from os import path
 from datetime import datetime
 from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
-from flask import Flask, render_template, redirect, request, url_for
+
+from flask import Flask, flash, render_template, redirect, request, url_for, session
+
+
 if path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
 
+
+"""
+Login to MongoDB
+"""
 app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DB')
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 datetime_now = datetime.now()  # pass this to a MongoDB doc
+
 
 
 @app.route('/')
@@ -44,7 +54,7 @@ def add_recipe():
 def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
-    return redirect(url_for('get_recipes'))
+    return redirect(url_for('get_index'))
 
 
 @app.route('/edit_recipe/<recipe_id>')
@@ -70,51 +80,14 @@ def update_recipe(recipe_id):
         'recipe_serv': request.form.get('recipe_serv'),
         'recipe_rate': request.form.get('recipe_rate')
     })
-    return redirect(url_for('get_recipes'))
+    recipe_id = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template("showOne.html", recipe=recipe_id)
 
 
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
-    return redirect(url_for('get_recipes'))
-
-
-@app.route('/get_categories')
-def get_categories():
-    return render_template('categories.html',
-                           categories=mongo.db.categories.find())
-
-
-@app.route('/delete_category/<category_id>')
-def delete_category(category_id):
-    mongo.db.categories.remove({'_id': ObjectId(category_id)})
-    return redirect(url_for('get_categories'))
-
-
-@app.route('/edit_category/<category_id>')
-def edit_category(category_id):
-    return render_template('editcategory.html',
-                           category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
-
-
-@app.route('/update_category/<category_id>', methods=['POST'])
-def update_category(category_id):
-    mongo.db.categories.update(
-        {'_id': ObjectId(category_id)},
-        {'category_name': request.form.get('category_name')})
-    return redirect(url_for('get_categories'))
-
-
-@app.route('/insert_category', methods=['POST'])
-def insert_category():
-    category_doc = {'category_name': request.form.get('category_name')}
-    mongo.db.categories.insert_one(category_doc)
-    return redirect(url_for('get_categories'))
-
-
-@app.route('/add_category')
-def add_category():
-    return render_template('addcategory.html')
+    return redirect(url_for('get_index'))
 
 
 @app.route('/show_recipe/<recipe_id>/')
@@ -159,6 +132,49 @@ def search_by_serv(recipe_serv):
                            results_total=results_total)
 
 
+"""
+Working on the Categories
+"""
+
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+                           category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
+
+
 @app.route('/find_item_cat/<category_name>', methods=['GET', 'POST'])
 def search_by_cat(category_name):
     db_query = {'category_name': category_name}
@@ -169,6 +185,11 @@ def search_by_cat(category_name):
                            results_total=results_total)
 
 
+"""
+Working on Shop/Item
+"""
+
+
 @app.route('/get_shop')
 def get_shop():
     return render_template("shop.html",
@@ -177,12 +198,14 @@ def get_shop():
 
 @app.route('/show_item/<item_id>/')
 def show_item(item_id):
-    """
-    The view that displays recipe information based on the recipe ID.
-    """
     item_id = mongo.db.shop.find_one({"_id": ObjectId(item_id)})
     return render_template("showItem.html", item=item_id)
-    
+
+
+"""
+Charts part
+"""
+
 
 @app.route('/see charts')
 def see_charts():
@@ -190,6 +213,7 @@ def see_charts():
 
 
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
